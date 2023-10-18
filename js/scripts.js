4aa9a7d7b656d415db8836e637183e5fefd9adf7
Vitaliy Sulimenko <prevetal@gmail.com>
2023-10-18 09:49:16 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,287 @@
+// Ширина окна для ресайза
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+OVERLAY = document.querySelector('.overlay')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Карусель акций
+	const stocksSliders = [],
+		stocks = document.querySelectorAll('.stocks .swiper')
+
+	stocks.forEach(function (el, i) {
+		el.classList.add('stocks_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 'auto',
+					spaceBetween: 24
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 24
+				},
+				1024: {
+					slidesPerView: 3,
+					spaceBetween: 24
+				},
+				1899: {
+					slidesPerView: 3,
+					spaceBetween: 20
+				}
+			}
+		}
+
+		stocksSliders.push(new Swiper('.stocks_s' + i, options))
+	})
+
+
+	// Фото галерея
+	const photoGallerySliders = [],
+		photoGallery = document.querySelectorAll('.photo_gallery .swiper')
+
+	photoGallery.forEach(function (el, i) {
+		el.classList.add('photo_gallery_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			slidesPerView: 1,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 24
+				},
+				768: {
+					spaceBetween: 60
+				},
+				1024: {
+					spaceBetween: 80
+				},
+				1280: {
+					spaceBetween: 124
+				},
+				1440: {
+					spaceBetween: 160
+				},
+				1899: {
+					spaceBetween: 173
+				}
+			}
+		}
+
+		photoGallerySliders.push(new Swiper('.photo_gallery_s' + i, options))
+	})
+
+
+	// Карусель файлов
+	const filesSliders = [],
+		files = document.querySelectorAll('.files .swiper')
+
+	files.forEach(function (el, i) {
+		el.classList.add('files_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			slidesPerView: 'auto',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 20
+				},
+				1280: {
+					spaceBetween: 23
+				}
+			}
+		}
+
+		filesSliders.push(new Swiper('.files_s' + i, options))
+	})
+
+
+	// Карусель акций
+	const bottomStocksSliders = [],
+		bottomStocks = document.querySelectorAll('.stocks_slider .swiper')
+
+	bottomStocks.forEach(function (el, i) {
+		el.classList.add('stocks_slider_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			slidesPerView: 'auto',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			},
+			spaceBetween: 24
+		}
+
+		bottomStocksSliders.push(new Swiper('.stocks_slider_s' + i, options))
+	})
+
+
+	// Мини всплывающие окна
+	$('.mini_modal_btn').click(function(e) {
+		e.preventDefault()
+
+		const modalId = $(this).data('modal-id')
+
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		} else {
+			$('.mini_modal_btn').removeClass('active')
+			$(this).addClass('active')
+
+			$('.mini_modal').removeClass('active')
+			$(modalId).addClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'pointer')
+		}
+	})
+
+	// Закрываем всплывашку при клике за её пределами
+	$(document).click(e => {
+		if ($(e.target).closest('.modal_cont').length === 0) {
+			$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		}
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+
+	// Увеличение картинки
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false,
+		},
+		Thumbs: {
+			autoStart: false,
+		}
+	})
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click(e => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').toggleClass('active')
+		$('body').toggleClass('menu_open')
+		$('header .menu').toggleClass('show')
+	})
+
+
+	// Маска ввода
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Перезапись ширины окна
+		WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+		// Моб. версия
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
